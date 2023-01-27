26bf4d4d9da82cd7670b3bda5dda1b56f4837000
Smooth-E <smoothie@disroot.org>
2023-01-27 19:20:55 +0500

    Use object declarations instead of classes with companion object in some cases

--- app/src/main/java/com/smoothie/wirelessDebuggingSwitch/WirelessDebugging.kt
+++ app/src/main/java/com/smoothie/wirelessDebuggingSwitch/WirelessDebugging.kt
@@ -4,27 +4,23 @@ import android.content.Context
 import android.net.wifi.WifiManager
 import android.text.format.Formatter
 
-class WirelessDebugging : SuperShell() {
+object WirelessDebugging : SuperShell() {
 
-    companion object {
-
-        var enabled: Boolean = false
-            get() = Integer.parseInt(execute("settings get global adb_wifi_enabled")) == 1
-            set(value) {
-                execute("settings put global adb_wifi_enabled ${if (value) 1 else 0}")
-                field = value
-            }
-
-        fun getPort(): String = execute("getprop service.adb.tls.port")
-
-        fun getAddress(context: Context): String {
-            val wm =
-                context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            val connectionInfo = wm.connectionInfo
-            val ipAddress = connectionInfo.ipAddress
-            return Formatter.formatIpAddress(ipAddress)
+    var enabled: Boolean = false
+        get() = Integer.parseInt(execute("settings get global adb_wifi_enabled")) == 1
+        set(value) {
+            execute("settings put global adb_wifi_enabled ${if (value) 1 else 0}")
+            field = value
         }
 
+    fun getPort(): String = execute("getprop service.adb.tls.port")
+
+    fun getAddress(context: Context): String {
+        val wm =
+            context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val connectionInfo = wm.connectionInfo
+        val ipAddress = connectionInfo.ipAddress
+        return Formatter.formatIpAddress(ipAddress)
     }
 
 }
--- app/src/main/java/com/smoothie/wirelessDebuggingSwitch/widget/RoundedWidgetUtilities.kt
+++ app/src/main/java/com/smoothie/wirelessDebuggingSwitch/widget/RoundedWidgetUtilities.kt
@@ -8,72 +8,68 @@ import android.widget.RemoteViews
 import com.smoothie.wirelessDebuggingSwitch.R
 import com.smoothie.wirelessDebuggingSwitch.Utilities
 
-class RoundedWidgetUtilities {
+object RoundedWidgetUtilities {
 
-    companion object {
+    fun applyRemoteViewsParameters(
+        context: Context,
+        preferences: SharedPreferences,
+        remoteViews: RemoteViews,
+        cornerViewIds: Array<Int> = arrayOf(
+            R.id.corner_bottom_left,
+            R.id.corner_bottom_right,
+            R.id.corner_top_left,
+            R.id.corner_top_right
+        ),
+        plainViewIds: Array<Int> = arrayOf(
+            R.id.side_top,
+            R.id.side_bottom,
+            R.id.side_left,
+            R.id.side_right,
+            R.id.center
+        )
+    ) {
+        val radius = Utilities.getWidgetCornerRadius(context, preferences)
 
-        fun applyRemoteViewsParameters(
-            context: Context,
-            preferences: SharedPreferences,
-            remoteViews: RemoteViews,
-            cornerViewIds: Array<Int> = arrayOf(
-                R.id.corner_bottom_left,
-                R.id.corner_bottom_right,
-                R.id.corner_top_left,
-                R.id.corner_top_right
-            ),
-            plainViewIds: Array<Int> = arrayOf(
-                R.id.side_top,
-                R.id.side_bottom,
-                R.id.side_left,
-                R.id.side_right,
-                R.id.center
-            )
-        ) {
-            val radius = Utilities.getWidgetCornerRadius(context, preferences)
-
-            val cornerBitmap = Utilities.generateWidgetCornerBitmap(context, preferences, radius)
-            cornerViewIds.forEach { id ->
-                remoteViews.setBitmap(id, "setImageBitmap", cornerBitmap)
-            }
-
-            val centerBitmap = Utilities.generateRectangleBitmapForWidget(context, preferences)
-            plainViewIds.forEach { id ->
-                remoteViews.setBitmap(id, "setImageBitmap", centerBitmap)
-            }
+        val cornerBitmap = Utilities.generateWidgetCornerBitmap(context, preferences, radius)
+        cornerViewIds.forEach { id ->
+            remoteViews.setBitmap(id, "setImageBitmap", cornerBitmap)
         }
 
-        fun applyPreviewParameters(
-            context: Context,
-            preferences: SharedPreferences,
-            view: View,
-            cornerViewIds: Array<Int> = arrayOf(
-                R.id.corner_bottom_left,
-                R.id.corner_bottom_right,
-                R.id.corner_top_left,
-                R.id.corner_top_right
-            ),
-            plainViewIds: Array<Int> = arrayOf(
-                R.id.side_top,
-                R.id.side_bottom,
-                R.id.side_left,
-                R.id.side_right,
-                R.id.center
-            )
-        ) {
-            val radius =  Utilities.getWidgetCornerRadius(context, preferences)
+        val centerBitmap = Utilities.generateRectangleBitmapForWidget(context, preferences)
+        plainViewIds.forEach { id ->
+            remoteViews.setBitmap(id, "setImageBitmap", centerBitmap)
+        }
+    }
 
-            val cornerBitmap = Utilities.generateWidgetCornerBitmap(context, preferences, radius)
-            cornerViewIds.forEach { id ->
-                view.findViewById<ImageView>(id).setImageBitmap(cornerBitmap)
-            }
+    fun applyPreviewParameters(
+        context: Context,
+        preferences: SharedPreferences,
+        view: View,
+        cornerViewIds: Array<Int> = arrayOf(
+            R.id.corner_bottom_left,
+            R.id.corner_bottom_right,
+            R.id.corner_top_left,
+            R.id.corner_top_right
+        ),
+        plainViewIds: Array<Int> = arrayOf(
+            R.id.side_top,
+            R.id.side_bottom,
+            R.id.side_left,
+            R.id.side_right,
+            R.id.center
+        )
+    ) {
+        val radius =  Utilities.getWidgetCornerRadius(context, preferences)
 
-            val centerBitmap = Utilities.generateRectangleBitmapForWidget(context, preferences)
-            plainViewIds.forEach { id ->
-                view.findViewById<ImageView>(id).setImageBitmap(centerBitmap)
-            }
+        val cornerBitmap = Utilities.generateWidgetCornerBitmap(context, preferences, radius)
+        cornerViewIds.forEach { id ->
+            view.findViewById<ImageView>(id).setImageBitmap(cornerBitmap)
         }
 
+        val centerBitmap = Utilities.generateRectangleBitmapForWidget(context, preferences)
+        plainViewIds.forEach { id ->
+            view.findViewById<ImageView>(id).setImageBitmap(centerBitmap)
+        }
     }
 
 }
